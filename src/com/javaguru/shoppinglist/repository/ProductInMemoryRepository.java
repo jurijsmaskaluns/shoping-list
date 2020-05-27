f9f281895fc24d9517e724a8dda500f4d1e21f71
JMJ <jurijsmaskaluns@gmail.com>
2020-05-27 18:01:27 +0300

    Branch: feature/3
    task 1 Product name cannot be less than 3 characters and more than 32. ok
    task 2 The discount should not be more than 100%. ok
    task 3 The discount cannot be less than 0. The discount may be a fractional number (10.5%). ok (discount type is integer)
    task 4 The price of the product must be greater than 0. ok
    task 7 If the price of the product is less than $ 20, then a discount cannot be made. ok
    task 6 The name of the product must be unique. failed

--- src/com/javaguru/shoppinglist/repository/ProductInMemoryRepository.java
+++ src/com/javaguru/shoppinglist/repository/ProductInMemoryRepository.java
@@ -13,6 +13,10 @@ public class ProductInMemoryRepository implements ProductRepository {
     @Override
     public Product save(Product product) {
         Product productCopy = new Product(productIdSequence++, product.getName(), product.getPrice(), product.getDiscount(), product.getCategory(), product.getDescription());
+        if(product.getPrice().intValue() <20 ){
+            Product productCopy1 = new Product(productCopy.getId(),productCopy.getName(), productCopy.getPrice(), 0, productCopy.getCategory(),productCopy.getDescription());
+            return productCopy1;
+        }
         productRepository.put(productCopy.getId(), productCopy);
         return productCopy;
     }
